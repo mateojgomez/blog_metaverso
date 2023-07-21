438797c7f0d663a5e3b1f59cebee9b2ffa930dd9
Mateo <mateo.gomezcma@gmail.com>
2023-07-21 17:15:58 -0300

    creacion de vistas noticas y categorias

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import agregar_noticias
-
+from .views import agregar_noticias , AgregarCategoria, AgregarNoticia
+#Ceci en clase utiliza las views como clases
 urlpatterns = [
-    path('administrar/noticias/agregar_noticias', agregar_noticias ,name='agregar_noticias'),
+    path('agregar_noticia', AgregarNoticia.as_view() ,name='agregar_noticia'),
+    path('agregar_categoria', AgregarCategoria.as_view() ,name='agregar_categoria'),
 ]
 
+#TODO continuar con la clase 7/7 desde el minuto 29
--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Categoria, Noticia
+#Ceci crea las Clases de vistas
+
+class AgregarCategoria(CreateView):
+    model= Categoria
+    fields=['nombre']
+    template_name='agregar_categoria.html'
+    succes_url = reverse_lazy('inicio')
+
+
+class AgregarNoticia(CreateView):
+    model= Noticia
+    fields=['titulo','creador','contenido','categoria']
+    template_name='agregar_noticias.html'
+    succes_url = reverse_lazy('inicio')
+
 
 
 def agregar_noticias(request):
