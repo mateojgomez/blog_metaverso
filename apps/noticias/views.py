7449b24fb9410ea818d84f09485a294984b0a433
federmon <fdrcmnzn1990@gmail.com>
2023-07-26 20:51:54 -0300

    vista "noticias" y cambio del boton eliminar

--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import FormNoticia
 from .models import Noticia
+from django.views.generic import ListView, DetailView
 
-def noticia(request):
-    noticias = noticias.objects.all()
-    return render (request, 'agregar_noticias.html',{'noticias': noticias})
+#def noticia(request):
+ #   noticias = noticias.objects.all()
+   # return render (request, 'agregar_noticias.html',{'noticias': noticias})
+
+
+class NoticiaListViewAdmin(ListView):
+    model = Noticia
+    template_name= " administrar_noticias.html "
+    context_object_name = "noticias"
+
+
+
+class NoticiaListView(ListView):
+    model = Noticia
+    template_name= " noticias/noticias.html "
+    context_object_name = "noticias"
+
+
+class NoticiaDetailView(DetailView):
+    model = Noticia
+    template_name = "noticias/noticia_individual.html"
+    context_object_name = "noticias"
+    pk_url_kwarg = "id"
+    queryset = Noticia.objects.all()
 
 def AgregarNoticia(request):
     data = {
@@ -34,7 +56,12 @@ def EditarNoticia(request,id):
     return render(request,'editar_noticia.html', data)
 
     
-def EliminarNoticia(request,id):
-    noticia = Noticia.objects.filter(id=id)
-    noticia.delete()
+def EliminarNoticia(request,):
+    id=request.GET.get("id",None)
+    
+    if id: 
+        noticia = Noticia.objects.filter(id=id)
+        noticia.delete()
     return redirect(to='administrar_noticias')   
+        
+        
--- blog_metaverso/urls.py
+++ blog_metaverso/urls.py
@@ -8,18 +8,21 @@ from .views import registrarse
 from .views import administrar_noticias
 from .views import administrar_categorias
 from apps.noticias.views import AgregarNoticia, EliminarNoticia, EditarNoticia
-#from .views import publicaciones
+from apps.noticias.views import NoticiaListView, NoticiaDetailView, NoticiaListViewAdmin
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('index/', index, name='index'),
     path('registrarse/', registrarse, name='registrarse'),
     # Ceci incluye las paths de las apps
     path('noticias/', include('apps.noticias.urls')),
-   # path('publicaciones/', publicaciones, name= 'publicaciones'),
-    path('administrar/noticias',administrar_noticias,name='administrar_noticias'),
+    path('administrar/noticias', administrar_noticias,name='administrar_noticias'),
     path('administrar/categorias',administrar_categorias,name='administrar_categorias'),
     path('administrar/noticias/agregar_noticias', AgregarNoticia,name='agregar_noticias'),
-    path('administrar/noticias/eliminar_noticia/<int:id>/', EliminarNoticia,name='eliminar_noticia'),
-    path('administrar/noticias/editar_noticia/<int:id>/', EditarNoticia, name='editar_noticia')
+    path('administrar/noticias/eliminar_noticia/', EliminarNoticia,name='eliminar_noticia'),
+    path('administrar/noticias/editar_noticia/<int:id>/', EditarNoticia, name='editar_noticia'),
+    
+    path('noticias/<int:id>/', NoticiaDetailView.as_view(), name = "noticia_individual"),
+    
 ]
