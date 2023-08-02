0381fe5018cf8f31c8c08f88c63df876242ace65
federmon <fdrcmnzn1990@gmail.com>
2023-08-02 10:17:49 -0300

    cards en noticias_list y administrar_noticias.

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('<int:id>/comentarios',ComentarioCreateView.as_view(), name = "comentarios"),
     path('administrar/', administrar_noticias,name='administrar_noticias'),
     path('agregar_noticias', AgregarNoticia ,name='agregar_noticias'),
-    path('noticias/administrar/eliminar_noticia/<int:id>/', EliminarNoticia,name='eliminar_noticia'),
+    path('eliminar_noticia/', EliminarNoticia,name='eliminar_noticia'),
     path('comentarios/',ComentarioCreateView.as_view(), name = "comentarios"),
     path('comentario/editar/<int:id>/', EditarComentario, name='editar_comentario')
     ]
